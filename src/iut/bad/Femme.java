6d2b9cf13449b03547ae091110b5d1f88e698f02
milanfandresena <milanfandresena28@gmail.com>
2025-08-19 15:08:41 +0300

    13 Dans la classe Femme, créez une méthode main() qui instancie un Homme et une Femme, et déclare

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -8,15 +8,12 @@ public class Femme extends Humain {
     }
 
     public static void main(String[] args) {
-        Femme f1 = new Femme("Rasoa", "Marie", 25);
-        Femme f2 = new Femme("Hanta", "Lina", 27);
-        Homme h1 = new Homme("Rakoto", "Jean", 30);
+        Homme h = new Homme("Rakoto", "Jean", 30);
+        Femme f = new Femme("Rasoa", "Marie", 25);
 
-        f1.ami(f2);
-        f1.ami(h1);
+        f.ami(h);  
 
-        f1.listerAmis();
-        f2.listerAmis();
-        h1.listerAmis();
+        f.listerAmis();  
+        h.listerAmis();  
     }
 }
